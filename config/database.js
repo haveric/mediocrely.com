3f2861aab71d1a46d781aeb797434d1710db6fb1
Ryan Breuer <breuerryan@gmail.com>
2020-09-10 14:32:13 -0500

    Remove environments folder

--- config/database.js
+++ config/database.js
@@ -4,12 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
+        client: env('DATABASE_CLIENT'),
+        host: env('DATABASE_HOST', ''),
+        port: env.int('DATABASE_PORT', 0),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool('DATABASE_SSL', false),
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {}
     },
   },
 });
--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b68b118a132660aecd6049de5574f052'),
+      secret: env('ADMIN_JWT_SECRET', ''),
     },
   },
 });
