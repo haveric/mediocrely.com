11caf97b076b941078bf8697f183d71e9e06ef6e
Ryan Breuer <breuerryan@gmail.com>
2022-10-29 00:20:01 -0500

    Update a few dependencies

--- public/src/App.js
+++ public/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import {Home} from './components/Home';
 import './App.scss';
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+  </Routes>
 );
 
 export default App;
