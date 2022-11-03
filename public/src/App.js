373d1e57a76fb07f60b8f3c286131a2b25b2fd18
Ryan Breuer <breuerryan@gmail.com>
2022-11-03 10:04:40 -0500

    Set max column width and fix App references

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import {HomePage} from './components/Home';
-import EventPageWithRouter from "./components/Event";
+import {HomePage} from './components/HomePage';
+import EventPageWithRouter from "./components/EventPage";
 import './App.scss';
 
 const App = () => (
